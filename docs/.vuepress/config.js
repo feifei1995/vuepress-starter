2d23585781c7fe3f15c0b7b18bb8f7f5e4e7282b
yifei <1499692281@qq.com>
2023-08-09 02:47:06 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,26 @@
 module.exports = {
   title: 'Hello zyf',
   description: 'Just playing around',
+  themeConfig: {
+    // navbar: false,
+    nav: [
+      { text: 'Home', link: '/' },
+      {
+        text: 'test',
+        ariaLabel: 'test Menu',
+        items: [
+          { text: 'bar', link: '/bar/' },
+          { text: 'foo', link: '/foo/' }
+        ]
+      },
+      { text: 'External', link: 'https://baidu.com' },
+    ],
+    sidebar: [
+      '/',
+      '/bar/',
+      ['/foo/', 'Explicit link text']
+    ]
+  },
   base: '/',
   port: 8009
 }
